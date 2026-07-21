53a5127afa444e9ccf84939d3f9190b1179375df
Lunasnow22 <daygod2019@gmail.com>
2026-07-21 15:56:06 +0700

    feat(week-02): migrate modern JavaScript lab

--- labs/week-02/source/vite.config.js
+++ labs/week-02/source/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 
-// TODO: เปลี่ยนให้ตรงชื่อ repository ของตนเอง เช่น engse203-lab02-67123456789
-const repositoryName = "engse203-lab02-68543210075-6";
+const repositoryName = "engse203-student-labs-68543210075-6";
 
 export default defineConfig({
-  base: `/${repositoryName}/`,
+  base: `/${repositoryName}/labs/week-02/`,
   build: {
     outDir: "docs",
+    emptyOutDir: true,
   },
 });
